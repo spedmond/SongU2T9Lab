41d4a54be6e9f7ca15a3c9204c74ac9f966387c3
unknown <BT_3W2_25@BKTECH.LOCAL>
2022-10-17 12:16:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+            LuckyNumbers hi = new LuckyNumbers();
+            String lucky = hi.getLuckyNumbers();
+            System.out.println(lucky);
+        }
+    }
